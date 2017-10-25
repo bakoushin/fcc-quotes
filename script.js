7740dc18ee45220842dc9f9b204ff0a39a4fd5d9
Alex Bakoushin <alex@bakoush.in>
2017-10-25 17:18:54 +0300

    Use Please.js for random color

--- script.js
+++ script.js
@@ -1,49 +1,18 @@
 $(function(){
 
-var getQuote = function() {
-
-    // object for storing all nessesary quote properties
-    var quote = {
-        "quoteReady": false,
-        "colorReady": false,
-        "quote": "",
-        "author": "",
-        "textColor": 0xFFF,
-        "backgroundColor": 0x000,
-        "ready": function() {
-            return this.quoteReady && this.colorReady;
-        }
-    };
-
-    // get random quote on design
+    $('#get-quote').click(function() {
     $.getJSON('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand', function(data) {
+        // update text
         var quoteObject = data[0];
-        quote.text = quoteObject.content;
-        quote.author = quoteObject.title;
-        quote.quoteReady = true;
-        updateQuote(quote);
+        quote = quoteObject.content;
+        author = quoteObject.title;
+        $('#quote__text').html(quote);
+        $('#quote__author').html(author);
+        // update color
+        var color = Please.make_color()[0]; 
+        $('#quote').css('color', color);
+        $('body').css('background-color', color);
     });
-
-    // get random color scheme
-    $.getJSON('http://www.colr.org/json/scheme/random', function(data) {
-        var colors = data.schemes[0].colors;
-        quote.backgroundColor = colors[colors.length - 1];
-        quote.textColor = colors[0];
-        quote.colorReady = true;
-        updateQuote(quote);
-    });
-
-}
-
-var updateQuote = function(quote) {
-    if (quote.ready()) {
-        $('#quote__text').html(quote.text);
-        $('#quote__author').html(quote.author);
-        $('#quote__text').css('color', quote.textColor);
-        $('body').css('background-color', quote.backgroundColor);
-    }
-}
-
-$('#get-quote').click(getQuote);
+});
 
 });    

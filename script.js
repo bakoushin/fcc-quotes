b6bbfa8636f6b01a3306505ae7854ee4d335860d
Alex Bakoushin <alex@bakoush.in>
2017-10-26 13:37:28 +0300

    Add styling

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 $(function(){
 
-    $('#get-quote').click(function() {
-    $.getJSON('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand', function(data) {
-        // update text
-        var quoteObject = data[0];
-        quote = quoteObject.content;
-        author = quoteObject.title;
-        $('#quote__text').html(quote);
-        $('#quote__author').html(author);
-        // update color
-        var color = Please.make_color()[0]; 
-        $('#quote').css('color', color);
-        $('body').css('background-color', color);
+    $('#button-update').click(function() {
+        $.getJSON('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&nocache=' + new Date().getTime(), function(data) {
+            // update text
+            var quoteObject = data[0];
+            var quote = quoteObject.content;
+            var author = quoteObject.title;
+            $('#quote__text').html(quote);
+            $('#quote__author').html(author);
+            // update color using Please.js
+            // http://www.checkman.io/please/
+            var color = Please.make_color()[0]; 
+            $('#quote').css({
+                "color": color,
+                "border-color": color
+            });
+            $('body').css('background-color', color);
+        });
     });
-});
 
 });    
